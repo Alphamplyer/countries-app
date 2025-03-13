6d938f9d8b484ef6ea0c888457b821ac4218bb69
Alphamplyer <even@pouyadou.fr>
2025-03-13 01:56:01 +0100

    fetch countries

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router";
 import type { Route } from "./+types/home";
+
+export async function clientLoader() {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+  return data;
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +14,22 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Countries() {
-  return <h1>Countries Page</h1>;
+export default function Countries({ loaderData }: Route.ComponentProps) {
+  console.log(loaderData);
+  return (
+    <div>
+      <ul>
+        {loaderData.map((country: any, key: any) => (
+          <li key={key}>
+            <Link to={`/countries/${country.name.common}`}>
+              {country.name.common}
+            </Link>
+            <div>
+              Region: {country.region} | Population {country.population}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
